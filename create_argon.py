48d33ad5fcb2c16cec4de42030644b14b8f12833
Ali <khsrali@gmail.com>
2019-12-07 15:15:09 +0100

    Argon_initial_positions

--- create_argon.py
+++ create_argon.py
@@ -3,7 +3,24 @@ from ase.build import bulk
 
 
 # latt const https://aip.scitation.org/doi/10.1063/1.1726009
-Ar = bulk('Ar', 'fcc',5.3118)
+# Ar = bulk('Ar', 'fcc',5.3118)
+# =======
+unitcell = bulk('Ar', 'fcc',5.3118,orthorhombic=True)
+from ase import visualize, Atoms
+
+# Ars= Atoms(bulk, size=(10,10,10))
+from ase.build import stack
+
+atoms_a = unitcell
+for i in range(10):
+    atoms_a = stack(atoms_a, unitcell, axis=0)
+atoms_b = atoms_a
+for j in range(8):
+    atoms_b = stack(atoms_b, atoms_a, axis=1)
+all_atoms = atoms_b
+for k in range(8):
+    all_atoms = stack(all_atoms, atoms_b, axis=2)
 
 
-print(Ar)
+
+visualize.view(all_atoms)
