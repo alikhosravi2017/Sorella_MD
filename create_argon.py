79008c23ae6621e7bdae6492e6bafee40892e2af
Ali <khsrali@gmail.com>
2019-12-07 15:48:49 +0100

    It gives you a XYZ output

--- create_argon.py
+++ create_argon.py
@@ -6,10 +6,11 @@ from ase.build import bulk
 # Ar = bulk('Ar', 'fcc',5.3118)
 # =======
 unitcell = bulk('Ar', 'fcc',5.3118,orthorhombic=True)
-from ase import visualize, Atoms
-
-# Ars= Atoms(bulk, size=(10,10,10))
+from ase import visualize
 from ase.build import stack
+from ase.io import write
+# Ars= Atoms(bulk, size=(10,10,10))
+
 
 atoms_a = unitcell
 for i in range(10):
@@ -21,7 +22,8 @@ all_atoms = atoms_b
 for k in range(8):
     all_atoms = stack(all_atoms, atoms_b, axis=2)
 
-
-
+# all_atoms.set_cell([all_atoms.get_cell()[0][0], all_atoms.get_cell()[1][1], all_atoms.get_cell()[2][2] ])
+write("initial_positions.xyz", all_atoms, format='xyz', parallel=True, append=False)
 
 visualize.view(all_atoms)
+print("the cell is=",all_atoms.get_cell())
