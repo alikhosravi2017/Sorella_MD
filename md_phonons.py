1c845a08602bdb0d1fbbd62cfa24af9c64a7c169
Nevensky <neven.golenic@gmail.com>
2020-02-06 18:02:19 +0100

    phonon code - problems
    cannot invert G, <Rq.Rq*> probably wrong....

--- md_phonons.py
+++ md_phonons.py
@@ -1,12 +1,13 @@
 #/usr/bin/env python3
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D 
 import matplotlib.pyplot as plt
 from numba import njit
 plt.style.use('ggplot')
 
 # parameters
 k_B = 1
-
+trajectory_file = "traj.xyz"
 
 # notation
 # l,l' -> refers to the unit cell in the supercell
@@ -19,11 +20,33 @@ l = [5,5,5]
 # origin of l-th unit cell relative to l=[0,0,0]
 r_l = np.dot(l,cell)
 
-@njit()
-def force_constants(G,T):
-	""" Calculates force constants $\Phi_{lk\alpha,l'k'\beta}$ """
-	phi = k_B * T* np.linalg.inv(G)
-	return phi
+
+def read_xyz(pos=trajectory_file):
+	""" Reads .xyz file and create a frame-indexed trajectory array."""
+	with open(pos,"r") as f:
+		lines = f.readlines()
+		frame = 0
+		k = 0
+		counter = 0
+		for idx,ln in enumerate(lines):
+			if counter==0:
+				Natoms = int(ln)
+				Nframes = int(len(lines)/(Natoms+2))
+				traj = np.zeros((Nframes,Natoms,4))
+				counter += 1
+			elif counter==1:
+				k = 0 # atom idx
+				counter +=1 
+			elif counter>1:
+				b = [float(l) for l in ln.split()]
+				traj[frame,k,:] = [float(l) for l in ln.split()]
+				k += 1
+				counter += 1
+				if counter==(Natoms+2):
+					counter = 0
+					frame += 1
+	return traj
+
 
 # @njit()
 def equidist(p1, p2, npoints=20):
@@ -59,14 +82,80 @@ def highsymm_path(symm_points,npoints=200):
 
 	return path
 
-a = 1. # lattice constant
-# high symmetry points for fcc Gamma -> X -> W -> K -> Gamma -> L
-gamma = np.array([0,0,0])
-X = np.array([0,2*np.pi/a,0])
-W = np.array([np.pi/a,2*np.pi/a,0])
-K =  np.array([3*np.pi/(2*a),3*np.pi/(2*a),0])
-L = np.array([np.pi/a,np.pi/a,np.pi/a])	
-pt = highsymm_path(np.array([gamma,X,W,K,gamma,L]))
-plt.plot(pt,"-.")
-plt.legend(["1/x","1/y","1/z"])
-plt.show()
+
+# @njit()
+def greens_func(traj):
+	"""	Takes the Fourier transform of the absolute positions for a given vector.
+	Averages all frames and calculates the FT Green's function coeffs at each 
+	wave vector q."""
+	# Nframes = G.shape[0]
+	# Natoms = G.shape[1]
+	R_ka = np.mean(traj,axis=0) # average over all frames
+
+	Natoms = R_ka.shape[0]
+	G_ft = np.zeros((Natoms,3,Natoms,3)) # ka, k'b
+	print(G_ft.shape)
+
+	for k1 in range(Natoms):
+		for k2 in range(Natoms):
+			for a in range(3):
+				for b in range(3):
+					G_ft[k1,a,k2,b] = np.fft.fft(R_ka[k1,a]*R_ka[k2,b])#-np.fft.fft(R_ka[k1,a])*np.fft.fft(R_ka[k2,b])
+
+	# R_ka_kb = R_ka*R_kb
+	# R_ka_ft = np.fft.fft(R_ka)
+	# R_kb_ft = np.conj(np.fft.fft(R_kb))
+	# R_ka_kb_ft = np.fft.fft(R_ka*R_kb)
+	# G_ft = np.zeros((Natoms,3,Natoms,3)) # ka, k'b
+	# G_ft = R_ka_kb_ft-R_ka_ft*R_kb_ft
+	print(G_ft.shape)
+	return G_ft
+
+# @njit()
+def force_constants(G,T=20):
+	""" Calculates force constants $\Phi_{lk\alpha,l'k'\beta}$ """
+	phi = k_B * T* np.linalg.inv(G,axes=(0,2))
+	return phi
+
+def eigenfreqs(traj,M=1):
+	G_ft = greens_func(traj)
+	# print(G_ft.shape)
+	# phi_ft = force_constants(G_ft)
+	phi_ft = np.zeros(G_ft.shape)
+	# print(G_ft.shape)
+	for k in range(G_ft.shape[0]):
+		for a in range(G_ft.shape[1]):
+			phi_ft[k,a] = force_constants(G_ft[k,a])
+	D = 1/np.sqrt(M)* phi_ft
+	omega = np.linalg.eig(D)
+	return np.sqrt(omega)
+
+
+def main():
+	a = 1. # lattice constant
+	# high symmetry points for fcc Gamma -> X -> W -> K -> Gamma -> L
+	gamma = np.array([0,0,0])
+	X = np.array([0,2*np.pi/a,0])
+	W = np.array([np.pi/a,2*np.pi/a,0])
+	K =  np.array([3*np.pi/(2*a),3*np.pi/(2*a),0])
+	L = np.array([np.pi/a,np.pi/a,np.pi/a])	
+	pt = highsymm_path(np.array([gamma,X,W,K,gamma,L]))
+	# plt.plot(pt,"-.")
+	# plt.legend(["1/x","1/y","1/z"])
+	# plt.show()
+
+
+
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	traj = read_xyz()[:,:,1:]
+
+	# Gf=greens_func(traj)
+	freqs = eigenfreqs(traj)
+	# ax.scatter(freqs[:,0],freqs[:,1],freqs[:,2])
+	# plt.show()
+
+	return None
+
+if __name__ == '__main__':
+	main()
--- plot_output.py
+++ plot_output.py
@@ -5,23 +5,23 @@ plt.style.use('ggplot')
 
 try:
     with open("output.dat","r") as f:
-    	#step,E_kin,E_pot,E_tot,temp_now
-    	data = np.loadtxt(f)
-    	fig, axs = plt.subplots(2, 1)
+        #step,E_kin,E_pot,E_tot,temp_now
+        data = np.loadtxt(f)
+        fig, axs = plt.subplots(2, 1)
 
-    	axs[0].plot(data[:,0],data[:,1],data[:,0],data[:,2],data[:,0],data[:,3])
-    	axs[0].set_ylim(top=4*np.min(data[:,1]),bottom=4*np.min(data[:,2]))
-    	axs[0].set_xlabel("step [a.u.]")
-    	axs[0].set_ylabel("Energy [a.u.]")
-    	axs[0].legend([r"$E_\mathrm{kin}$",r"$E_\mathrm{pot}$",r"$E_\mathrm{tot}$"])
+        axs[0].plot(data[:,0],data[:,1],data[:,0],data[:,2],data[:,0],data[:,3])
+        # axs[0].set_ylim(top=5*np.max(data[:,1]),bottom=5*np.min(data[:,2]))
+        axs[0].set_xlabel("step [a.u.]")
+        axs[0].set_ylabel("Energy [a.u.]")
+        axs[0].legend([r"$E_\mathrm{kin}$",r"$E_\mathrm{pot}$",r"$E_\mathrm{tot}$"])
 
-    	axs[1].plot(data[:,0],data[:,4])
-    	axs[1].set_ylim(bottom=0.)
-    	axs[1].set_xlabel("step [a.u.]")
-    	axs[1].set_ylabel("Temperature [ K ]")
-    	# print(np.std(data[10:,4])) # std deviation of temperature
-    	fig.tight_layout()
-    	plt.show()
+        axs[1].plot(data[:,0],data[:,4])
+        axs[1].set_ylim(bottom=0.)
+        axs[1].set_xlabel("step [a.u.]")
+        axs[1].set_ylabel("Temperature [ K ]")
+        # print(np.std(data[10:,4])) # std deviation of temperature
+        fig.tight_layout()
+        plt.show()
 except IOError:
     print("File not accessible")
 
